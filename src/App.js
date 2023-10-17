34911095a924499186317f3b9dd3f1d749afd9d7
Anirban-45 <azadarannya@gmail.com>
2023-10-17 09:35:26 +0600

    packages edited

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
             <Router>
                 <Navbar/>
                 <Routes>
-                    <Route path="/Portfolio" element={<Home/>} />
+                    <Route path="/" element={<Home/>} />
                     <Route path="/about" element={<About/>} />
                     <Route path="/work" element={<Work/>} />
                     <Route path="/contact" element={<Contact/>} />
